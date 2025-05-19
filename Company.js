399b70fc846b97e1354d2a933caea547f5ce566a
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-05-19 12:50:55 +0300

    Editorial
    Removed BS AND RBS functions
    Added Comments for public display

--- Company.js
+++ Company.js
@@ -1,31 +1,3 @@
-function bubblesort(arr) {
-  let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
-
-function reverseBubblesort(arr) {
-  let n = arr.length;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - 1 - i; j++) {
-      if (arr[j] < arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
-    }
-  }
-  return arr;
-}
-
 let companies = [
   {
     name: "Huge Corporate LTD",
